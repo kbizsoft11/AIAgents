06e7feb5f309f0e0fbcfdbcc7239dfd2c3a01581
kbizsoft11 <details.kbiz@gmail.com>
2026-08-13 17:47:37 +0530

    Add security enhancements and JavaScript obfuscation

---
+++ Colix-Al-Chrome-Addon/obfuscator.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  compact: true,
+  controlFlowFlattening: true,
+  controlFlowFlatteningThreshold: 0.75,
+  deadCodeInjection: true,
+  deadCodeInjectionThreshold: 0.4,
+  identifierNamesGenerator: 'hexadecimal',
+  renameGlobals: false,
+  stringArray: true,
+  stringArrayEncoding: ['base64'],
+  stringArrayThreshold: 1,
+  selfDefending: true,
+}
